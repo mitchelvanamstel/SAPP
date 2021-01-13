9a2f60c31bbfedbf29827d4bbc9b6e09090c7a22
Mitchel van Amstel <m.vanamstel@dutchcrypto.works>
2021-01-13 13:41:01 +0100

    Revert "adds lock to avoid deadlocks"
    This reverts commit e95e10d4e02f4fef4f077757aa4ac8ee7676032e.

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -854,10 +854,7 @@ void ThreadCheckMasternodes()
             MilliSleep(1000);
             boost::this_thread::interruption_point();
             // try to sync from all available nodes, one step at a time
-            {
-                LOCK(cs_main);
-                masternodeSync.Process();
-            }
+            masternodeSync.Process();
 
             if (masternodeSync.IsBlockchainSynced()) {
                 c++;
